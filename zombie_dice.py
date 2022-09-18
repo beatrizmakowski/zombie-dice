d50b38d288e785b9d25341054ad108d2e11ac96e
Beatriz Makowski <bemakow@gmail.com>
2022-09-18 15:26:31 -0300

    Refatoração para segunda entrega

--- zombie_dice.py
+++ zombie_dice.py
@@ -3,7 +3,7 @@ Implementação em Python do jogo Zombie Dice
 Aluna: Beatriz Makowski (RA 1112022201591)
 Curso: Superior de Tecnologia em Análise de Desenvolvimento de Sistemas
 Arquivo criado em: 07/08/2022
-Última alteração: 27/08/2022
+Última alteração: 18/09/2022
 
 Readme disponível em: https://github.com/beatrizmakowski/zombie-dice#readme
 '''
@@ -14,21 +14,32 @@ import random
 
 # <--- Variáveis globais --->
 
-NUMERO_DE_JOGADORES = 0
-JOGADOR_ATUAL = 0
-VENCEDOR = None
+numero_de_joagores = 0
+jogador_atual = 0
+vencedor = None
+jogador = [] # Lista para armazenar os jogadores
+dado_verde = ('C', 'P', 'C', 'T', 'P', 'C')
+dado_amarelo = ('T', 'P', 'C', 'T', 'P', 'C')
+dado_vermelho = ('T', 'P', 'T', 'C', 'P', 'T')
+numero_de_dados_para_sortear = 3
 
-''' Lista para armazenar os jogadores '''
-JOGADORES = []
+def inicializar_copo_dados():
+    ''' Inicializa o copo de dados com 6 dados verdes, 4 dados amarelos e 3 dados vermelhos '''
+    global copo_dados
 
-'''  Lista para representar o tubo onde onde serão colocados os treze dados inicialmente '''
-TODOS_OS_DADOS = ['CPCTPC', 'CPCTPC', 'CPCTPC', 'CPCTPC', 'CPCTPC', 'CPCTPC', # 6 dados verdes 
-                  'TPCTPC', 'TPCTPC', 'TPCTPC','TPCTPC', # 4 dados amarelos
-                  'TPTCPT', 'TPTCPT', 'TPTCPT'] # 3 dados vermelhos
-                
+    copo_dados = []
+    
+    for _ in range(6):
+        copo_dados.append(dado_verde)
+    for _ in range(4):
+        copo_dados.append(dado_amarelo)
+    for _ in range(3):
+        copo_dados.append(dado_vermelho)
 
-'''  Lista que será manipulada para desconsiderar os dados já sorteados na jogada atual '''
-DADOS_DISPONIVEIS = TODOS_OS_DADOS
+'''  Lista para representar o copo onde onde serão colocados os treze dados inicialmente '''
+copo_dados = []
+inicializar_copo_dados()       
+dados_disponiveis = copo_dados # Lista que será manipulada para desconsiderar os dados já sorteados na jogada atual
 
 
 # <--- Classes personalizadas --->
@@ -47,7 +58,7 @@ class Format:
 
 
 class Jogador:
-    ''' Classe criada para facilitar o controle das informações dos jogadores '''
+    ''' Classe criada para facilitar o controle das informações dos jogador '''
 
     def __init__(self, numero, nome) -> None:
         self.numero = numero
@@ -63,6 +74,10 @@ class Jogador:
         self.pegadas = 0
 
 
+class CopoVazio(Exception):
+    pass
+
+
 # <--- Funções personalizadas --->
 
 ''' Limpa a tela do terminal '''
@@ -76,28 +91,28 @@ def customPrint(text, type='', NEGRITO=False):
     print(f'{font_weight}{type}{text}{Format.END}')
 
 
-def obter_numero_de_jogadores():
-    ''' Pergunta ao usuário o número de jogadores, repetindo até que o input seja um número maior ou igual a 2 '''
+def obter_numero_de_joagores():
+    ''' Pergunta ao usuário o número de jogador, repetindo até que o input seja um número maior ou igual a 2 '''
 
-    global NUMERO_DE_JOGADORES
+    global numero_de_joagores
     while True:
         try:
-            NUMERO_DE_JOGADORES = int(input(f'{Format.NEGRITO}{Format.ROSA}\n'
+            numero_de_joagores = int(input(f'{Format.NEGRITO}{Format.ROSA}\n'
                                             f'Quantas pessoas irão jogar? '
                                             f'{Format.END}'))
         except ValueError:
             customPrint('Não entendi... (╥_╥) Por favor, digite apenas números inteiros.',
                         type=Format.AMARELO)
         else: 
-            if NUMERO_DE_JOGADORES >= 2:
+            if numero_de_joagores >= 2:
                 break
-            customPrint('O número mínimo de jogadores é 2!', type=Format.AMARELO)
+            customPrint('O número mínimo de jogador é 2!', type=Format.AMARELO)
 
 
-def obter_nomes_dos_jogadores():
-    ''' Pergunta ao usuário o número de jogadores, repetindo até que o input seja uma string válida '''
+def obter_nomes_dos_jogador():
+    ''' Pergunta ao usuário o número de jogador, repetindo até que o input seja uma string válida '''
 
-    for i in range(NUMERO_DE_JOGADORES):
+    for i in range(numero_de_joagores):
         while True:
             try:
                 nome = input(f'{Format.NEGRITO}{Format.ROSA}'
@@ -107,32 +122,75 @@ def obter_nomes_dos_jogadores():
                 customPrint('Não entendi... (╥_╥) Por favor, digite novamente.', type=Format.AMARELO)
             else:
                 novoJogador = Jogador(i + 1, nome)
-                JOGADORES.append(novoJogador)
+                jogador.append(novoJogador)
                 break
 
 
+def imprimir_dados_disponiveis_no_copo():
+    ''' Imprime na saída do console quais os dados que estão armazenados no copo em cada uma das jogadas (Checklist Semana 5) '''
+
+    numero_de_dados_verdes = numero_de_dados_amarelos = numero_de_dados_vermelhos = 0
+    plural_dados_verdes = plural_dados_amarelos = plural_dados_vermelhos = ''
+
+    for dado in copo_dados:
+        if dado == dado_verde:
+            numero_de_dados_verdes += 1
+        elif dado == dado_amarelo:
+            numero_de_dados_amarelos += 1
+        elif dado == dado_vermelho:
+            numero_de_dados_vermelhos += 1
+
+    if numero_de_dados_verdes > 1:
+        plural_dados_verdes = 's'
+    elif numero_de_dados_verdes == 0:
+        numero_de_dados_verdes = 'nenhum'
+
+    if numero_de_dados_amarelos > 1:
+        plural_dados_amarelos = 's'
+    elif numero_de_dados_amarelos == 0:
+        numero_de_dados_amarelos = 'nenhum'
+
+    if numero_de_dados_vermelhos > 1:
+        plural_dados_vermelhos = 's'
+    elif numero_de_dados_vermelhos == 0:
+        numero_de_dados_vermelhos = 'nenhum'
+
+    customPrint(f'\nDados disponíveis no copo: {Format.NEGRITO}{Format.VERDE}{numero_de_dados_verdes} dado{plural_dados_verdes} verde{plural_dados_verdes}{Format.END}, '
+                f'{Format.NEGRITO}{Format.AMARELO}{numero_de_dados_amarelos} dado{plural_dados_amarelos} amarelo{plural_dados_amarelos}{Format.END} e '
+                f'{Format.NEGRITO}{Format.VERMELHO}{numero_de_dados_vermelhos} dado{plural_dados_vermelhos} vermelho{plural_dados_vermelhos}{Format.END}.')
+
+
 def sortear_dados(numero_de_sorteios):
-    ''' Usa a biblioteca random para sortear dados dentre as opções disponíveis na variável global DADOS_DISPONIVEIS
+    ''' Usa a biblioteca random para sortear dados dentre as opções disponíveis na variável global copo_dados
     Imprime no terminal quantos dados de cada cor foram sorteados.
     '''
 
-    global DADOS_DISPONIVEIS
+    global copo_dados
+
+    if len(copo_dados) == 0:
+        customPrint(f'\nO copo está vazio!', NEGRITO=True, type=Format.VERMELHO) 
+        raise CopoVazio
+    elif len(copo_dados) < 3:
+        numero_de_sorteios = len(copo_dados)
+
     dados = []
     numero_de_dados_verdes = numero_de_dados_amarelos = numero_de_dados_vermelhos = 0
     plural_dados_verdes = plural_dados_amarelos = plural_dados_vermelhos = ''
 
+    imprimir_dados_disponiveis_no_copo()
     customPrint('\nSorteando dados...')
 
     for _ in range(numero_de_sorteios):
 
-        dado = random.choice(DADOS_DISPONIVEIS)
-        dados.append(dado)
+        dado = random.choice(copo_dados) # Escolhe um dado do copo
+        copo_dados.pop(copo_dados.index(dado)) # Remove o dado escolhido do copo
+        dados.append(dado) # Adiciona o dado escolhido na lista de dados que serão rolados para sortear uma face
 
-        if dado == 'CPCTPC':
+        if dado == dado_verde:
             numero_de_dados_verdes += 1
-        elif dado == 'TPCTPC':
+        elif dado == dado_amarelo:
             numero_de_dados_amarelos += 1
-        elif dado == 'TPTCPT':
+        elif dado == dado_vermelho:
             numero_de_dados_vermelhos += 1
 
     if numero_de_dados_verdes > 1:
@@ -163,8 +221,8 @@ def jogar_dados(dados):
     ''' Usa a biblioteca random para sortear uma face de cada dado sorteado pela função sortear_dados()
     Atualiza as informações do jogador atual
     '''
-    global DADOS_DISPONIVEIS
-    global JOGADORES
+    global dados_disponiveis
+    global jogador
     cores = []
     resultados = []
 
@@ -173,23 +231,26 @@ def jogar_dados(dados):
 
     for dado in dados:
 
-        if dado == 'CPCTPC': # Dado verde
+        if dado == dado_verde:
             cores.append(Format.VERDE)
-        elif dado == 'TPCTPC': # Dado amarelo
+        elif dado == dado_amarelo:
             cores.append(Format.AMARELO)
-        else: # Dado vermelho
+        else:
             cores.append(Format.VERMELHO)
 
         resultado = random.choice(dado)
         resultados.append(resultado)
 
-        if resultado == 'P': # [P] = pegadas
-            JOGADORES[JOGADOR_ATUAL].pegadas += 1
-            DADOS_DISPONIVEIS.append(dado) # Os dados [P] podem ser sorteados novamente se o jogador quiser continuar
-        elif resultado == 'C': # [C] = cérebros
-            JOGADORES[JOGADOR_ATUAL].cerebros += 1
-        elif resultado == 'T': # [T] = tiros
-            JOGADORES[JOGADOR_ATUAL].tiros += 1
+        # 'P' == pegada
+        # 'C' == cérebro
+        # 'T' == tiro
+        if resultado == 'P': 
+            jogador[jogador_atual].pegadas += 1
+            dados_disponiveis.append(dado) # Os dados que caírem na face 'P' podem ser sorteados na próxima jogada, se o jogador quiser continuar
+        elif resultado == 'C': 
+            jogador[jogador_atual].cerebros += 1
+        elif resultado == 'T': 
+            jogador[jogador_atual].tiros += 1
     
     imprimir_dados_jogados(cores, resultados)
 
@@ -232,25 +293,29 @@ def imprimir_placar():
     Verifica se há um vencedor (jogador com 13 pontos ou mais)
     Caso sim, chama a função de encerrar o jogo.
     '''
-    
-    global VENCEDOR
-    customPrint('\nPlacar:')
 
-    for j in range(NUMERO_DE_JOGADORES):
-        customPrint(f'{JOGADORES[j].nome}: {JOGADORES[j].pontos}')
+    global vencedor
 
-        if JOGADORES[j].pontos >= 13:
-            VENCEDOR = JOGADORES[j]
+    titulo = '*** Placar ***'
+    customPrint(f'\n{titulo:^20}', NEGRITO=True, type=Format.ROSA)
+    customPrint(f'{"Jogador":^10}|{"Pontos":^10}', NEGRITO=True)
+    customPrint('-'*21)
 
-    if VENCEDOR:
+    for j in range(numero_de_joagores):
+        customPrint(f'{jogador[j].nome:^10}| {jogador[j].pontos:^10}')
+
+        if jogador[j].pontos >= 13:
+            vencedor = jogador[j]
+
+    if vencedor:
         encerrar_o_jogo()
 
 
 def encerrar_o_jogo():
     ''' Imprime a mensagem de encerramento e encerra a execução do programa '''
 
-    customPrint(f'\nUau! {VENCEDOR.nome} ganhou o jogo com {VENCEDOR.pontos} pontos! \(•◡•)/', type=Format.ROSA, NEGRITO=True)
-    input(f'{Format.ROSA}\n[Pressione qualquer tecla para encerrar o jogo]\n{Format.END}')
+    customPrint(f'\nUau! {vencedor.nome} ganhou o jogo com {vencedor.pontos} pontos! Parabéns! \(•◡•)/', type=Format.ROSA, NEGRITO=True)
+    input(f'{Format.ROSA}\n\n[Pressione qualquer tecla para encerrar o jogo]\n{Format.END}')
     quit()
 
 
@@ -260,57 +325,63 @@ clear()
 customPrint('** Bem vindo(a) ao jogo Zombie dados! \(•◡•)/ **', type=Format.ROSA, NEGRITO=True)
 customPrint('[Pressione Ctrl+Z a qualquer momento para sair do jogo]', type=Format.ROSA)
 
-obter_numero_de_jogadores()
-obter_nomes_dos_jogadores()
+obter_numero_de_joagores()
+obter_nomes_dos_jogador()
 
 clear()
 customPrint('\nComeçando uma nova partida! \(•◡•)/', type=Format.ROSA, NEGRITO=True)
 
 while True:
-    for i in range(NUMERO_DE_JOGADORES):
-        JOGADOR_ATUAL = i
+    for i in range(numero_de_joagores):
+        jogador_atual = i
         jogador_quer_continuar = True
 
         imprimir_placar()
         
-        customPrint(f'\n{JOGADORES[i].nome}, é a sua vez!\n', NEGRITO=True, type=Format.ROSA)
-        DADOS_DISPONIVEIS = TODOS_OS_DADOS
+        customPrint(f'\n{jogador[i].nome}, é a sua vez!\n', NEGRITO=True, type=Format.ROSA)
         input(f'{Format.ROSA}[Pressione qualquer tecla sortear os dados]\n{Format.END}')
-        numero_de_dados_para_sortear = 3
         clear()
 
         while jogador_quer_continuar:
             plural_pontos = plural_tiros = ''
 
-            sortear_dados(numero_de_dados_para_sortear)
+            try:
+                sortear_dados(numero_de_dados_para_sortear)
+            except CopoVazio:
+                jogador_quer_continuar = False
+                input(f'{Format.ROSA}\n[Passando a vez para o próximo jogador]\n{Format.END}')
+                continue
 
             customPrint(f'\nSua pontuação atual é: \n')
             customPrint(f'{"Cérebros":^10}|{"Pegadas":^10}|{"Tiros":^10}')
-            customPrint(f'{JOGADORES[i].cerebros:^10}|{JOGADORES[i].pegadas:^10}|{JOGADORES[i].tiros:^10}')
+            customPrint(f'{jogador[i].cerebros:^10}|{jogador[i].pegadas:^10}|{jogador[i].tiros:^10}')
 
-            if JOGADORES[i].tiros >= 3:
-                customPrint(f'\nAh não! Você levou {JOGADORES[i].tiros} tiros e perdeu todos os pontos acumulados nesta '
+            if jogador[i].tiros >= 3:
+                customPrint(f'\nAh não! Você levou {jogador[i].tiros} tiros e perdeu todos os pontos acumulados nesta '
                             f'rodada! (╥_╥)', NEGRITO=True, type=Format.VERMELHO)           
-                JOGADORES[i].cerebros = 0
+                jogador[i].cerebros = 0
                 jogador_quer_continuar = False
                 input(f'{Format.ROSA}\n[Pressione qualquer tecla para continuar]\n{Format.END}')
                 clear()
                 continue
             
-            if JOGADORES[i].cerebros > 1:
+            if jogador[i].cerebros > 1:
                 plural_pontos = 's'
-            if 3 - JOGADORES[i].tiros > 1:
+            if 3 - jogador[i].tiros > 1:
                 plural_tiros = 's'
 
             customPrint(f'\nVocê pode encerrar esta rodada e ganhar '
-                        f'{Format.NEGRITO}{Format.VERDE}{JOGADORES[i].cerebros} ponto{plural_pontos}{Format.END} '
-                        f'ou jogar novamente e tentar aumentar seu pontos!')
+                        f'{Format.NEGRITO}{Format.VERDE}{jogador[i].cerebros} ponto{plural_pontos}{Format.END} '
+                        f'ou jogar novamente e tentar aumentar sua pontuação!')
             customPrint(f'Porém, se você levar mais '
-                        f'{Format.NEGRITO}{Format.VERMELHO}{3 - JOGADORES[i].tiros} tiro{plural_tiros}{Format.END}, '
+                        f'{Format.NEGRITO}{Format.VERMELHO}{3 - jogador[i].tiros} tiro{plural_tiros}{Format.END}, '
                         f'sua vez acabará e você perderá todos os pontos que acumulou até agora...')
 
             jogador_quer_continuar = confirmar_se_jogador_quer_continuar()
             clear()
         
-        JOGADORES[i].pontos += JOGADORES[i].cerebros
-        JOGADORES[i].resetar_cerebros_tiros_pegadas()
+        inicializar_copo_dados()
+        dados_disponiveis = copo_dados
+
+        jogador[i].pontos += jogador[i].cerebros
+        jogador[i].resetar_cerebros_tiros_pegadas()
